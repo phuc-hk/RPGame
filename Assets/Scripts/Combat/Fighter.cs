89863083cdd68a8f73f894501ff562f6ed5a503f
Phuc Hoang <phuc.hoangkien@gmail.com>
2023-08-10 16:59:54 +0700

    feat: add attack animation and attack throttling

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -8,11 +8,18 @@ namespace RPGame.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
-        Transform target;
         [SerializeField] float weaponRange;
+        [SerializeField] float timeBetweenAttack = 3.0f;
+
+        float timeSinceLastAttack = 0;
+        Transform target;
         bool isInrange;
+
         private void Update()
-        {          
+        {
+            timeSinceLastAttack += Time.deltaTime;
+            
+
             if (target != null)
             {
                 isInrange = Vector3.Distance(transform.position, target.position) < weaponRange;
@@ -23,18 +30,34 @@ namespace RPGame.Combat
                 else
                 {
                     GetComponent<Mover>().Cancel();
+                    AttackBehavior();
                 }
             }          
         }
+
+        private void AttackBehavior()
+        {
+            if (timeSinceLastAttack > timeBetweenAttack)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }       
+        }
+
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            target = combatTarget.transform;
+            target = combatTarget.transform;           
         }
 
         public void Cancel()
         {
            target = null;
+        }
+
+        void Hit()
+        {
+
         }
     }
 }
