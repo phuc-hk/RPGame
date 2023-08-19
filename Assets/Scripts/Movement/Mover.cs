4ef37b0e537c1eb2c982cf56113f40df105ab4cd
Phuc Hoang <phuc.hoangkien@gmail.com>
2023-08-19 16:57:06 +0700

    feat: saving and loading player position

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,5 +1,6 @@
 using RPGame.Combat;
 using RPGame.Core;
+using RPGame.Saving;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using UnityEngine.AI;
 
 namespace RPGame.Movement
 {
-    public class Mover : MonoBehaviour, IAction
+    public class Mover : MonoBehaviour, IAction, ISaveable
     {
         [SerializeField] float maxSpeed;
         NavMeshAgent navMeshAgent;
@@ -51,6 +52,19 @@ namespace RPGame.Movement
         public void Cancel()
         {
             navMeshAgent.isStopped = true;
+        }
+
+        public object CaptureState()
+        {
+           return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 }
